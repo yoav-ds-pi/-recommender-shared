3b5480597db267a983915ebe1eca1ef80b2959b9
yoav-ds-pi <yhaimovitch@powerinbox.com>
2021-06-28 18:08:17 +0300

    base_level = logging.WARNING

--- jeeng/shared/logging.py
+++ jeeng/shared/logging.py
@@ -1,9 +1,9 @@
 import logging
 
 
-def setup_logger():
+def setup_logger(base_level = logging.WARNING):
     logging.basicConfig()
-    logging.root.setLevel(logging.NOTSET)
-    logging.basicConfig(level=logging.NOTSET)
+    logging.root.setLevel(level=base_level)
+    logging.basicConfig(level=base_level)
     logger = logging.getLogger(__name__)
     logger.info('logger started')
