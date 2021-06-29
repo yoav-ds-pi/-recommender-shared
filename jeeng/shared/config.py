e218dcc61e5f01039788a1352c6436d26423a812
yoav-ds-pi <yhaimovitch@powerinbox.com>
2021-06-29 11:10:11 +0300

    base index names

--- jeeng/shared/config.py
+++ jeeng/shared/config.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+BASE_INDEX_SCORED_URLS = 'scored_urls'
+BASE_INDEX_URL_USER = 'url_user_index'
 
 @dataclass(frozen=True)
 class StressTestConfig:
@@ -14,8 +16,8 @@ class StressTestConfig:
 
 
 STRESS_SMALL = StressTestConfig(
-    URL_USER_INDEX='url_user_index_synt_small',
-    SCORED_URLS_INDEX='scored_urls_synt_small',
+    URL_USER_INDEX=BASE_INDEX_URL_USER+'_synt_small',
+    SCORED_URLS_INDEX=BASE_INDEX_SCORED_URLS+'_synt_small',
     N_DOMAINS=2,
     N_URLS=100,
     N_SUBS=200,
@@ -24,8 +26,8 @@ STRESS_SMALL = StressTestConfig(
 )
 
 STRESS_LARGE = StressTestConfig(
-    URL_USER_INDEX='url_user_index_synt_large',
-    SCORED_URLS_INDEX='scored_urls_synt_large',
+    URL_USER_INDEX=BASE_INDEX_URL_USER+'_synt_large',
+    SCORED_URLS_INDEX=BASE_INDEX_SCORED_URLS+'_synt_large',
     N_DOMAINS=4,
     N_URLS=5000,
     N_SUBS=5000,
