d2388e150a089685a78c84534043910e29bdef8f
yoav-ds-pi <yhaimovitch@powerinbox.com>
2021-07-06 13:29:00 +0300

    renamed

--- jeeng/shared/clog.py
+++ jeeng/shared/clog.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def setup_logger(base_level = logging.WARNING):
+def setup_logger(base_level=logging.WARNING):
     logging.basicConfig()
     logging.root.setLevel(level=base_level)
     logging.basicConfig(level=base_level)
