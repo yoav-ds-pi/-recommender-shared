9c44160ff4e76143508fc3f6825be8ebfd3c5ea5
yoav-ds-pi <yhaimovitch@powerinbox.com>
2021-07-06 13:47:22 +0300

    get_logger

--- jeeng/shared/clog.py
+++ jeeng/shared/clog.py
@@ -1,9 +1,16 @@
+from typing import Union
 import logging
 
 
-def setup_logger(base_level=logging.WARNING):
+def setup_logger(base_level: Union[int, str] = logging.WARNING):
     logging.basicConfig()
     logging.root.setLevel(level=base_level)
     logging.basicConfig(level=base_level)
     logger = logging.getLogger(__name__)
     logger.info('logger started')
+
+
+def get_logger(name: str, base_level: Union[int, str] = logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(base_level)
+    return logger
