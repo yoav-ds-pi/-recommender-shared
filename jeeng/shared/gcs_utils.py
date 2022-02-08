7865e3d288254f3f8231f2d9d625678c9da4a866
yoav-ds-pi <yhaimovitch@powerinbox.com>
2022-02-08 12:49:48 +0200

    added timeouts

--- jeeng/shared/gcs_utils.py
+++ jeeng/shared/gcs_utils.py
@@ -1,12 +1,16 @@
+import timeout_decorator
+
 from jeeng.shared.clog import get_logger
 import pickle
 from google.cloud import storage
+
+from jeeng.shared.time_mac import Timer
 
 
 def list_objects(bucket_name: str, prefix: str) -> list[storage.blob]:
     files = []
     client = storage.Client()
-    all_blobs = list(client.list_blobs(bucket_name, prefix=prefix + '/', delimiter='/'))
+    all_blobs = list(client.list_blobs(bucket_name, prefix=prefix + '/', delimiter='/', timeout=60))
 
     for blob in all_blobs:
         if blob.name[-3:] == 'csv':
@@ -15,33 +19,38 @@ def list_objects(bucket_name: str, prefix: str) -> list[storage.blob]:
     return files
 
 
-def write_df_to_dbq(df,full_table_name):
+def write_df_to_dbq(df, full_table_name: str):
     logger = get_logger("write_df_to_dbq")
-    try:
+
+    @timeout_decorator.timeout(60)
+    def do_write():
         df.to_gbq(destination_table=full_table_name, project_id='jeeng-275018', if_exists='append')
-    except Exception as exc:
+
+    try:
+        logger.info(f'to_gbq {full_table_name=}')
+        timer = Timer(f'{full_table_name=} to_gbq')
+        do_write()
+        logger.info(f'{timer.stop_round()}')
+    except Exception:
         logger.error('gcs_utils.write_df_to_gcs', exc_info=True)
 
 
-def store_pickle(obj,bucket,prefix,file_name):
+def store_pickle(obj, bucket_name: str, prefix: str, file_name: str):
     pkl_file = pickle.dumps(obj)
     client = storage.Client()
-    bucket = client.get_bucket(bucket)
+    bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(prefix + '/' + file_name)
-    gcs_writer = storage.blob.BlobWriter(blob)
-    gcs_writer.write(pkl_file)
-    gcs_writer.close()
+    with storage.blob.BlobWriter(blob, timeout=60) as gcs_writer:
+        gcs_writer.write(pkl_file)
+    # gcs_writer.close()
 
 
-def read_pickle(bucket,prefix,file_name):
+def read_pickle(bucket_name: str, prefix: str, file_name: str):
     client = storage.Client()
-    bucket = client.get_bucket(bucket)
-
-    blob = bucket.blob(prefix + '/' + file_name )
-    is_exists = blob.exists(client)
-    if is_exists:
-        obj = blob.download_as_bytes(client)
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(prefix + '/' + file_name)
+    if blob.exists(client):
+        obj = blob.download_as_bytes(client, timeout=60)
         pkl_obj = pickle.loads(obj)
         return pkl_obj
-    else:
-        return None
+    return None
